7b5e69c2f2f53ebdc8e69dd2fc20639e6b60b09a
3120005043_张俊鸿 <1017328759@qq.com>
2023-08-10 16:49:24 +0800

    first commit

---
+++ lab/lab0/lab00/lab00.py
@@ -0,0 +1,14 @@
+# x = 20
+# print(x)
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    return 20+200
+
+if __name__ == '__main__':
+    # assert twenty_twenty() == 200, '返回值必须是200'
+    twenty_twenty()
